9b69323cedf6b3530ef7794b4d7dc700b297b44c
Daniel Minschinski <danielminschinski@gmail.com>
2019-09-27 20:50:10 +0200

    Update index.html, style.css

--- js/script.js
+++ js/script.js
@@ -102,31 +102,3 @@ document.addEventListener('DOMContentLoaded', function(){
   todoColumn.addCard(card1);
   doingColumn.addCard(card2);
 });
-
-
-
-
-
-
-
-
-
-
-
-/*var table = {
-  name: 'project',
-  element: <Node element>
-};
-
-var column = {
-  id: '12j82da20k',
-  name: 'todo',
-  element: <Node element> // for example document.createElement('div')
-};
-
-var card = {
-  id: '2kd8s958ka',
-  description: 'Create Kanban app',
-  color: 'green',
-  element: <Node element>
-};*/
